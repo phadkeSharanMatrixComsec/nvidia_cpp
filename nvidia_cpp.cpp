a17a66a8e6f5235f47be63cbd4f4b246e6d01177
sharanphadke <sharan.phadke@matrixcomsec.com>
2023-03-20 09:23:32 +0530

    UPDATE: minor changes in nvidia_cpp.cpp

--- nvidia_cpp.cpp
+++ nvidia_cpp.cpp
@@ -11,10 +11,10 @@ int main()
     system("nvidia-smi --query-gpu=name --format=csv,noheader");
 
     #elif __APPLE__
-    system("ioreg -l | grep model > nvidia.txt");
+    system("ioreg -l | grep model");
 
     #elif WIN32
-    system("wmic path win32_VideoController get name > nvidia.txt");
+    system("wmic path win32_VideoController get name");
 
     #else
     cout <<"some other OS!" <<endl;
